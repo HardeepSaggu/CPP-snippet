39230bf2318715c696d02c1c00a5b12aefe02677
Hardeep Singh <31860413+HardeepSaggu@users.noreply.github.com>
2020-04-23 01:11:20 +0530

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+#include <functional> 
+#define int long long 
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+using namespace __gnu_pbds; 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> hashing;
+int pow(int a,int b,int mod = 8*1e18){int res = 1;while(b){if (b&1)res = (a*res)%mod;b/=2,a=(a*a)%mod;}return res%mod;}
+const int MOD = 1e9 + 7;
+
+/////////////////////////////////////" Please Don't ruin it "///////////////////////////////////
+
+
+
+
+
+int32_t main(){
+
+
+
+    return 0;
+}
